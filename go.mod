319db5fea3a05d7ff7ad1b2666f74641f0614cc0
Emerald <emerald.developer@proton.me>
2025-09-04 11:45:15 +0530

    final commit for branch

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module terminus
+module github.com/WhyIsEmerald/Terminus
 
 go 1.24.4
--- internals/units/metric.go
+++ internals/units/metric.go
@@ -1,1 +1,27 @@
 package units
+
+import (
+	"fmt"
+
+	"github.com/WhyIsEmerald/Terminus/data"
+)
+
+func Convert(Measurement string, initial string, target string, magnitude float64) {
+	unitsMap, err := data.ReadUnits(Measurement)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if _, ok := unitsMap[initial]; !ok {
+		fmt.Printf("Invalid initial unit: %s\n", initial)
+		return
+	}
+	if _, ok := unitsMap[target]; !ok {
+		fmt.Printf("Invalid target unit: %s\n", target)
+		return
+	}
+	initialUnit := unitsMap[initial].(float64)
+	targetUnit := unitsMap[target].(float64)
+	result := magnitude * initialUnit / targetUnit
+	fmt.Printf("%f %s = %f %s\n", magnitude, initial, result, target)
+}
